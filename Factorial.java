e51e519e45472adb2cd5c359bc615798ca704b8c
Singara <98253836+Singara@users.noreply.github.com>
2022-01-28 22:56:57 +0530

    Add files via upload

---
+++ Factorial.java
@@ -0,0 +1,20 @@
+package week1assignment;
+
+public class Factorial {
+	public static void main(String arg[]) {
+		int i,fact=1;
+		int number=5;//it is number to calculate factorial
+		for(i=1;i<=number;i++) {
+		fact=fact*i;
+	}
+	System.out.println("Factorial of "+number+" is:"+fact);
+	
+	
+
+	
+		
+		
+	}	
+	}
+
+
